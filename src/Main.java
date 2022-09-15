46cde2ba854838cef4b8579c5dd2c3f0014d710e
ShuEugene <shueugene@hotmail.com>
2022-09-15 07:32:25 +0300

    HW6.1.1,2,3 complete.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println();
-//  ДЗ 6.1.1,2
+//  ДЗ 6.1.1,2,3
         {
             int[] integerNumbers = new int[3];
             for (int i = 0; i < 3; i++) integerNumbers[i] = i+1;
@@ -11,12 +11,16 @@ public class Main {
             showArray(integerNumbers);
             showArray(floatNumbers);
             showArray(wordArray);
+            System.out.println();
+            showReverseArray(integerNumbers);
+            showReverseArray(floatNumbers);
+            showReverseArray(wordArray);
         }
 
         System.out.println();
     }
 
-// ДЗ6.1.2
+// ДЗ6.1.2,3
     static void showArray(int[] someArray) {
         for (int i = 0; i < someArray.length - 1; i++) System.out.printf("%d, ", someArray[i]);
         System.out.printf("%d.\n", someArray[someArray.length - 1]);
@@ -29,4 +33,17 @@ public class Main {
         for (int i = 0; i < someArray.length - 1; i++) System.out.printf("%s ", someArray[i]);
         System.out.printf("%s.\n", someArray[someArray.length - 1]);
     }
+    static void showReverseArray(int[] someArray) {
+        for (int i = someArray.length - 1; i > 0; i--) System.out.printf("%d, ", someArray[i]);
+        System.out.printf("%d.\n", someArray[0]);
+    }
+    static void showReverseArray(float[] someArray) {
+        for (int i = someArray.length - 1; i > 0; i--) System.out.printf("%.3f, ", someArray[i]);
+        System.out.printf("%.3f.\n", someArray[0]);
+    }
+    static void showReverseArray(String[] someArray) {
+        for (int i = someArray.length - 1; i > 0; i--) System.out.printf("%s ", someArray[i]);
+        System.out.printf("%s.\n", someArray[0]);
+    }
+
 }
